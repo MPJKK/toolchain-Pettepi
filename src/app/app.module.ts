a20fa85f158a658a5cceca3dbae9afc3a09ada26
Petteri Piironen <pettepi@ety-a2123b-14.edu.wks.metropolia.fi>
2018-01-18 14:36:29 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    NavigationComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
